bb5b5c1613afca88d83062776107b367572745ff
Ratnesh Kumar Dheeraj <parallelratnesh@gmail.com>
2019-02-27 09:51:31 +0545

    SQL Server Str

--- src/jdbc/ConnectSQLServer.java
+++ src/jdbc/ConnectSQLServer.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class ConnectURL {
+public class ConnectSQLServer {
 
     public static void main(String[] args) throws ClassNotFoundException {
 
